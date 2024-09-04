35e372356b0a259dadf2c840b9f26fcce46b01d5
Natalia Kuchta <n.kuchta@onet.pl>
2024-09-04 13:35:35 +0400

    my app Nike added lgo and sections project in react

---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from "./Hero.jsx";
+import PopularProducts from "./PopularProducts.jsx";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer.jsx";
+import Subscribe from "./Subscribe.jsx";
+import Services from "./Services.jsx";
+import Footer from "./Footer.jsx";
+import CustomerReviews from "./CustomerReviews.jsx";
+
+
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    SpecialOffer,
+    Subscribe,
+    Services,
+    Footer,
+    CustomerReviews,
+}
